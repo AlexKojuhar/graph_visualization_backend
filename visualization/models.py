c88b38a65607b6c1663d6f4d87b15834bfe94388
Alexandra Kozhuhar <kozuharalexandra@gmail.com>
2020-02-02 23:28:11 +0200

    Fixed serializers

--- visualization/models.py
+++ visualization/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Node(models.Model):
-    name = models.CharField(max_length=225)
+    name = models.CharField(unique=True, max_length=225)
+
+    def __str__(self):
+        return self.name
 
 class Link(models.Model):
     source = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='source')
--- visualization/serializers.py
+++ visualization/serializers.py
@@ -1,14 +1,23 @@
 from .models import Node, Link
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
-class NodeSerializer(serializers.HyperlinkedModelSerializer):
+class NodeSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=225, validators=[UniqueValidator(queryset=Node.objects.all())])
+
     class Meta:
         model = Node
-        fields = ['id', 'name']
+        fields = '__all__'
 
 
-class LinkSerializer(serializers.HyperlinkedModelSerializer):
+class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = ['id', 'target', 'source']
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super(LinkSerializer, self).to_representation(instance)
+        rep['source'] = instance.source.name
+        rep['target'] = instance.target.name
+        return rep
